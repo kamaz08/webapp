8f270071cbce740e6e898615311990bf69735466
kamaz <kamaz08@gmail.com>
2017-09-14 11:38:14 +0200

    Start

---
+++ WebApi/WebApi.Model/Model/Test/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Model.Model.Test
+{
+    public class Test
+    {
+        public String Message {get;set;}
+
+        public Test()
+        {
+            Message = "Przykładowy tekst.";
+        }
+    }
+}
